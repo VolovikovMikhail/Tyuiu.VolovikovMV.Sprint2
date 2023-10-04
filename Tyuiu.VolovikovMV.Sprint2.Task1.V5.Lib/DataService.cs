2155a961968b85f4fd5ac863f05c6ad0c0a6da1f
VolovikovMikhail <volovikov.1995@bk.ru>
2023-10-04 18:55:27 +0500

    Task1

---
+++ Tyuiu.VolovikovMV.Sprint2.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.VolovikovMV.Sprint2.Task1.V5.Lib
+{
+    public class DataService : ISprint2Task1V5
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a < b) | (c < d);
+            res[1] = (a + 1 == b) & (c == d);
+            res[2] = (a == b) || (c == d);
+            res[3] = (a == b) && (c < d);
+            res[4] = !(!res[0]);
+            res[5] = (a > b) ^ (c > d);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VolovikovMV.Sprint2.Task1.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint2.Task1.V5.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint2.Task1.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 154, b = 163, c = 134, d = 137;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a,b,c,d);
+            Console.Title = "Спринт #2 | Выполнил: Воловиков М. В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Воловиков Михаил Васильевич | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие: Написать программу из операций сравнений                       *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность можно чередовать, но          *");
+            Console.WriteLine("* использовать один раз в выражении) и логических операций                *");
+            Console.WriteLine("* (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться), *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет логическую             *");
+            Console.WriteLine("* последовательность(массив): (True, False, False, False, True, False)    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* a = 154, b = 163, c = 134, d = 137                                      *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
