706a8a8f4c2687c9dba026b468a8337b6bbe4485
VolovikovMikhail <volovikov.1995@bk.ru>
2023-10-14 19:44:38 +0500

    Task6

---
+++ Tyuiu.VolovikovMV.Sprint2.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VolovikovMV.Sprint2.Task6.V7.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint2.Task6.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 5;
+            int startYear = 1990;
+            string res = ds.FindMonthName(startYear, n);
+            string wait = "июнь";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VolovikovMV.Sprint2.Task6.V7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint2.Task6.V7.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint2.Task6.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Воловиков М. В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Воловиков Михаил Васильевич | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие: С начала 1990 года по некоторый день прошло n месяцев и 2 дня. *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите год:");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сколько месяцев прошло:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + ds.FindMonthName(g, m));
+            Console.ReadKey();
+        }
+    }
+}
